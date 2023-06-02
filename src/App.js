3eaa06b6062029b2012a7155068db493131e6fff
Aditya Adhikari <hemantaad@gmail.com>
2023-06-02 14:18:02 +0545

    Added navbar component and routing to various components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
-import Movies from "./components/movies";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/common/navbar";
+import Movies from "./components/movies";
+import NotFound from "./components/common/notFound";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import MovieForm from "./components/movieForm";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <main className="container">
+          <NavBar />
+          <Switch>
+            <Route path="/movies" exact component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" compo nent={Rentals} />
+            <Route
+              path="/movies/:id"
+              render={(props) => <MovieForm {...props} />}
+            />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
